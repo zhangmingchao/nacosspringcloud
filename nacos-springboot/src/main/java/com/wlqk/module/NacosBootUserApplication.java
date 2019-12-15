692b5a38691d34f813839d88a2fe96efc69f48d3
zc <1539835012@qq.com>
2019-12-15 20:15:12 +0800

    生产者和提供者

--- nacos-springboot/src/main/java/com/wlqk/module/NacosBootUserApplication.java
+++ nacos-springboot/src/main/java/com/wlqk/module/NacosBootUserApplication.java
@@ -3,11 +3,22 @@ package com.wlqk.module;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 //@NacosPropertySource(dataId = "example", autoRefreshed = true)
 @EnableDiscoveryClient
 public class NacosBootUserApplication {
+
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
 
     public static void main(String[] args) {
         SpringApplication.run(NacosBootUserApplication.class, args);
---
+++ nacos-springboot/src/main/java/com/wlqk/module/controller/TestController.java
@@ -0,0 +1,17 @@
+package com.wlqk.module.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class TestController {
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @GetMapping("/test")
+    public String test(){
+        return  restTemplate.getForObject("http://nacos-provider/get", String.class);
+    }
+}
